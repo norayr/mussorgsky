7df70efb8dbecf6186dda1d6fb108731e3921e14
Ivan Frade <ivan.frade@gmail.com>
2009-08-20 00:16:11 +0300

    Added new file in setup.py

--- setup.py
+++ setup.py
@@ -10,13 +10,14 @@ DATA = [('share/applications/hildon', ['data/mussorgsky.desktop']),
         ('share/dbus-1/services', ['data/mussorgsky.service']),
         ('share/pixmaps',['data/mussorgsky-icon.png']),
         ('lib/mussorgsky', ['src/album_art.py',
-                            'src/edit_panel.py',
-                            'src/mutagen_backend.py',
-                            'src/tracker_backend.py',
+                            'src/album_art_panel.py',
                             'src/album_art_spec.py',
+                            'src/download_dialog.py',
+                            'src/edit_panel.py',
                             'src/mussorgsky.py',
+                            'src/mutagen_backend.py',                            
                             'src/player_backend.py',
-                            'src/download_dialog.py'])]
+                            'src/tracker_backend.py'])]
  
 setup(name         = 'mussorgsky',
       version      = '0.1',
